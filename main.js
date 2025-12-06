efae19ab60a226c47739388a4540590ff87cb39a
Sagar Nepali <sagarnepali0960@gmail.com>
2025-12-06 13:01:48 +0545

    added navbar

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
+const cursor = document.getElementById("cursor");
+let mouseX = 0,
+  mouseY = 0;
+let currX = 0,
+  currY = 0;
 
-const cursor = document.getElementById('cursor');
-  let mouseX = 0, mouseY = 0;
-  let currX = 0, currY = 0;
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
 
-  document.addEventListener('mousemove', (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-  });
+function animateCursor() {
+  const speed = 0.5;
+  currX += (mouseX - currX) * speed;
+  currY += (mouseY - currY) * speed;
 
-  function animateCursor() {
-    const speed = 0.5;
-    currX += (mouseX - currX) * speed;
-    currY += (mouseY - currY) * speed;
+  cursor.style.transform = `translate(${currX}px, ${currY}px)`;
+  requestAnimationFrame(animateCursor);
+}
 
-    cursor.style.transform = `translate(${currX}px, ${currY}px)`;
-    requestAnimationFrame(animateCursor);
-  }
+animateCursor();
 
-  animateCursor();
+const contact = document.getElementById("contact");
+const contactBox = document.getElementById("contactBox");
+const contactBtn = document.getElementById("contactBtn");
 
+contact.addEventListener("mouseover", () => {
+  contact.style.fontWeight = "bold";
+  contact.style.fontSize = "1.3em";
+});
 
+contact.addEventListener("click", () => {
+  contactBox.style.display = "flex";
+  contactBox.style.visibility = "visible"
+  contactBox.style.transition = "0.5s ease-in";
+  contact.style.fontWeight = "bold";
+  contact.style.fontSize = "1.2em";
+});
+
+contactBtn.addEventListener("click", () => {
+  contact.style.fontWeight = "normal";
+  contact.style.fontSize = "1em";
+  contactBox.style.display = "none";
+  contactBox.style.transition = "0.5s ease-out";
+});
